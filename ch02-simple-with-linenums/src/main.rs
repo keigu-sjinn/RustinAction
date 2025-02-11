b498fe48c227c783b9a008e5ce9c45a6bc816962
keigu-sjinn <keigu2008@outlook.com>
2025-02-11 15:48:42 +0800

    ch02-simple-linenumbers finished

--- ch02-simple-with-linenums/src/main.rs
+++ ch02-simple-with-linenums/src/main.rs
@@ -1,3 +1,16 @@
 fn main() {
-    println!("Hello, world!");
+    let search_term = "picture";
+    let quote = "\
+Every face, every shap, bedroom windows, public-house, and
+dark square is a picture feverishly turned--in search of what?
+It is the same with books.
+What do we seek through millions of pages?";
+
+    let mut line_num: usize = 1;
+    for line in quote.lines() {
+        if line.contains(search_term) {
+            println! {"{}: {}",line_num,line}
+        }
+        line_num += 1;
+    }
 }
