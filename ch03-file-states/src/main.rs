5d7f01df8cc202685acad181c718d024c0091b98
keigu-sjinn <keigu2008@outlook.com>
2025-02-16 10:51:11 +0800

    ch03-file-states finished

---
+++ ch03-file-states/src/main.rs
@@ -0,0 +1,67 @@
+#[derive(Debug, PartialEq)]
+enum FileState {
+    Open,
+    Closed,
+}
+
+#[derive(Debug)]
+struct File {
+    name: String,
+    data: Vec<u8>,
+    state: FileState,
+}
+
+impl File {
+    fn new(name: &str) -> Self {
+        File {
+            name: String::from(name),
+            data: Vec::new(),
+            state: FileState::Closed,
+        }
+    }
+
+    fn new_with_data(name: &str, data: &Vec<u8>) -> Self {
+        let mut file = File::new(name);
+        file.data = data.clone();
+        file
+    }
+
+    fn read(&self, save_to: &mut Vec<u8>) -> Result<usize, String> {
+        let mut tmp = self.data.clone();
+        let read_length = tmp.len();
+
+        save_to.reserve(read_length);
+        save_to.append(&mut tmp);
+
+        Ok(read_length)
+    }
+}
+
+fn open(mut file: File) -> Result<File, String> {
+    file.state = FileState::Open;
+    Ok(file)
+}
+
+fn close(mut file: File) -> Result<File, String> {
+    file.state = FileState::Closed;
+    Ok(file)
+}
+
+fn main() {
+    let mut f5 = File::new("f5.txt");
+    let mut buffer: Vec<u8> = vec![];
+
+    if f5.read(&mut buffer).is_err() {
+        println!("Error checking is working");
+    }
+
+    f5 = open(f5).unwrap();
+    let f5_length = f5.read(&mut buffer).unwrap();
+    f5 = close(f5).unwrap();
+
+    let text = String::from_utf8_lossy(&buffer);
+
+    println!("{:?}", f5);
+    println!("{} is {} bytes long", &f5.name, f5_length);
+    println!("{}", text);
+}
